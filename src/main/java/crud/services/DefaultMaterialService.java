b91c4efadecead300435458e5e66bb125fb98a20
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-04-14 09:18:38 +0200

    Create DefaultMaterialService.java

---
+++ src/main/java/crud/services/DefaultMaterialService.java
@@ -0,0 +1,61 @@
+package crud.services;
+
+import crud.repository.MaterialRepository;
+import entity.Material;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class DefaultMaterialService implements DaoMaterialService {
+
+    private MaterialRepository materialRepository;
+
+    public DefaultMaterialService(MaterialRepository materialRepository) {
+        this.materialRepository = materialRepository;
+    }
+
+    @Override
+    public void add(String nameOfMaterial) {
+        Material newMaterial = Material.builder().name(nameOfMaterial).build();
+        materialRepository.save(newMaterial);
+    }
+
+    @Override
+    public void update(Long id, String afterChange) {
+        Material foundMaterial = materialRepository.findById(id).get();
+        foundMaterial.setName(afterChange);
+        materialRepository.save(foundMaterial);
+    }
+
+    @Override
+    public void remove(Long id) {
+        materialRepository.delete(materialRepository.findById(id).get());
+    }
+
+    @Override
+    public Material findById(Long id) {
+        Optional<Material> foundMaterial = materialRepository.findById(id);
+        if (materialRepository.findById(id).isPresent()) {
+            return foundMaterial.get();
+        } else {
+            return Material.builder().build();
+        }
+    }
+
+    @Override
+    public List findByName(String name) {
+        if (!materialRepository.findByName(name).isEmpty()) {
+            return materialRepository.findByName(name);
+        }else {
+            return new ArrayList();
+        }
+    }
+
+    @Override
+    public List selectList()
+    {
+        return materialRepository.findAll();
+    }
+}
