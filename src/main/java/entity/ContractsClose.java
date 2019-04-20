8843baf732609c32a151df4098582bae31c95907
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-04-20 22:15:08 +0200

    Update ContractsClose.java

--- src/main/java/entity/ContractsClose.java
+++ src/main/java/entity/ContractsClose.java
@@ -3,10 +3,8 @@ package entity;
 import lombok.Builder;
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 @Data
 @Entity
 @Builder(toBuilder = true)
@@ -14,15 +12,12 @@ public class ContractsClose {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long idSell;
-    private Long idCustomer;
-    private String customerName;
-    private String materialName;
-    private int nrTruck;
-    private int nrTruckContract;
-    private int amount;
-    private String contractName;
-    private int openClose;
-    private int idContractBuy;
-    private int idContractSell;
+    @ManyToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "idContractBuy")
+    @Column(name = "contractsOpenBuy", nullable = false, columnDefinition = "int default 0")
+    private ContractsOpenBuy contractsOpenBuy;
+    @ManyToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "idContractSell")
+    @Column(name = "contractsOpenBuy", nullable = false, columnDefinition = "int default 0")
+    private ContractsOpenSell contractsOpenSell;
 }
