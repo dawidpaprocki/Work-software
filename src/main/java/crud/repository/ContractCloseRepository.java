0a0fd71aa9157ecd38a639910fd2a7e13dcdafc6
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-05-09 22:30:40 +0200

    updated ContractClose

--- src/main/java/crud/repository/ContractCloseRepository.java
+++ src/main/java/crud/repository/ContractCloseRepository.java
@@ -1,10 +1,15 @@
 package crud.repository;
 
 import crud.model.ContractsClose;
+import crud.model.ContractsOpenBuy;
+import crud.model.ContractsOpenSell;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ContractCloseRepository extends JpaRepository<ContractsClose, Long> {
-
+    Optional<ContractsClose> findByContractsOpenBuy(ContractsOpenBuy contractsOpenBuy);
+    Optional<ContractsClose> findByContractsOpenSell(ContractsOpenSell contractsOpenSell);
 }
--- src/main/java/crud/services/interfaces/ContractsCloseService.java
+++ src/main/java/crud/services/interfaces/ContractsCloseService.java
@@ -1,14 +1,18 @@
 package crud.services.interfaces;
 
 import crud.model.ContractsClose;
+import crud.model.ContractsOpenBuy;
+import crud.model.ContractsOpenSell;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ContractsCloseService<T> {
+public interface ContractsCloseService {
     void addUpdate(ContractsClose contractsClose);
     void deleteContract(Long id);
     Optional<ContractsClose> findById(Long id);
+    Optional<ContractsClose> findByContractBuy(ContractsOpenBuy contractsOpenBuy);
+    Optional<ContractsClose> findByContractSell(ContractsOpenSell contractsOpenSell);
     List<ContractsClose> findAll();
 
 
