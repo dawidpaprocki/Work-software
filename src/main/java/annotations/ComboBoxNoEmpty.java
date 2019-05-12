bb5272efd1c3f787fcacf686325405277130fa80
Dawid Paprocki <dawid.paprocki90@gmail.com>
2019-05-12 22:47:12 +0200

    annotation validation for  ComboBoxs

---
+++ src/main/java/annotations/ComboBoxNoEmpty.java
@@ -0,0 +1,19 @@
+package annotations;
+
+import annotations.validations.ComboBoxNoEmptyValidator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ComboBoxNoEmptyValidator.class)
+public @interface ComboBoxNoEmpty {
+    String message();
+    Class[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/annotations/validations/ComboBoxNoEmptyValidator.java
@@ -0,0 +1,19 @@
+package annotations.validations;
+
+import annotations.ComboBoxNoEmpty;
+import javafx.scene.control.ComboBox;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Optional;
+
+public class ComboBoxNoEmptyValidator implements ConstraintValidator<ComboBoxNoEmpty, ComboBox> {
+
+   public void initialize(ComboBoxNoEmpty constraint) {
+   }
+
+
+   public boolean isValid(ComboBox comboBox, ConstraintValidatorContext context) {
+      return  Optional.ofNullable(comboBox.getValue()).isPresent();
+   }
+}
